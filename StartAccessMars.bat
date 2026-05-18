9992a42dfde653679e601381a876e3a67a6310dd
Crux0103 <thomaskay91@googlemail.com>
2026-05-18 16:40:06 +0100

    Added separate cave output window
    Start Access Mars Bat now opens both the main window and cave output window. Start the experience on both windows, wait for the intro to finish and then when you move in the main window it moves in the cave output version.

--- StartAccessMars.bat
+++ StartAccessMars.bat
@@ -1,10 +1,19 @@
 @echo off
+title Access Mars Launcher
 cd /d "%~dp0"
 
+echo Starting Access Mars server...
 start "" cmd /k "npm run start"
 
+echo Waiting for server to start...
 timeout /t 8 /nobreak >nul
 
+echo Opening controller window...
 start "" "http://localhost:3002"
+
+timeout /t 6 /nobreak >nul
+
+echo Opening CAVE output window...
+start "" "http://localhost:3002/?caveOutput=1"
 
 exit
--- src/js/equirectangular-output.js
+++ src/js/equirectangular-output.js
@@ -1,26 +1,45 @@
+var Scene = require('./core/scene').Scene;
+
 AFRAME.registerComponent('equirectangular-output', {
   init: function () {
+    var self = this;
+
+    this.sceneEl = this.el;
+    this.THREE = window.THREE;
+
+    this.params = new URLSearchParams(window.location.search);
+    this.isCaveOutput = this.params.get('caveOutput') === '1';
+
     this.enabledOutput = false;
-
-    var sceneEl = this.el;
-    var THREE = window.THREE;
-
-    this.sceneEl = sceneEl;
-    this.THREE = THREE;
+    this.latestSyncData = null;
+    this.lastRequestedSite = null;
 
     this.outputWidth = 4096;
     this.outputHeight = 2048;
     this.cubeSize = 1024;
 
-    this.cubeCamera = new THREE.CubeCamera(0.1, 100000, this.cubeSize);
-    sceneEl.object3D.add(this.cubeCamera);
+    this.channel = null;
 
-    this.quadScene = new THREE.Scene();
-    this.quadCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+    if ('BroadcastChannel' in window) {
+      this.channel = new BroadcastChannel('access-mars-sync');
 
-    var geometry = new THREE.PlaneBufferGeometry(2, 2);
+      this.channel.onmessage = function (event) {
+        if (!self.isCaveOutput) return;
+        self.latestSyncData = event.data;
+      };
+    } else {
+      console.warn('[EquirectangularOutput] BroadcastChannel not supported.');
+    }
 
-    var material = new THREE.ShaderMaterial({
+    this.cubeCamera = new this.THREE.CubeCamera(0.1, 100000, this.cubeSize);
+    this.sceneEl.object3D.add(this.cubeCamera);
+
+    this.quadScene = new this.THREE.Scene();
+    this.quadCamera = new this.THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+
+    var geometry = new this.THREE.PlaneBufferGeometry(2, 2);
+
+    var material = new this.THREE.ShaderMaterial({
       uniforms: {
         cubemap: { value: this.cubeCamera.renderTarget.texture }
       },
@@ -54,44 +73,143 @@ AFRAME.registerComponent('equirectangular-output', {
       depthTest: false
     });
 
-    this.quad = new THREE.Mesh(geometry, material);
+    this.quad = new this.THREE.Mesh(geometry, material);
     this.quadScene.add(this.quad);
-
-    var self = this;
 
     window.addEventListener('keydown', function (event) {
       if (event.key === 'e' || event.key === 'E') {
         self.enabledOutput = !self.enabledOutput;
-
         console.log('[EquirectangularOutput] Enabled:', self.enabledOutput);
-
-        if (self.enabledOutput) {
-          sceneEl.renderer.setSize(self.outputWidth, self.outputHeight, false);
-        }
       }
     });
 
-    console.log('[EquirectangularOutput] Component loaded. Press E inside Mars.');
+    if (this.isCaveOutput) {
+      this.enabledOutput = true;
+      console.log('[EquirectangularOutput] CAVE output mode active.');
+    } else {
+      console.log('[EquirectangularOutput] Normal controller mode active.');
+    }
+  },
+
+  tick: function () {
+    var playerEl = document.querySelector('#player');
+    var cameraEl = document.querySelector('#camera');
+
+    if (!playerEl || !cameraEl) return;
+
+    if (!this.isCaveOutput) {
+      this.broadcastNormalView(playerEl, cameraEl);
+    } else {
+      this.applyCaveSync(playerEl, cameraEl);
+    }
   },
 
   tock: function () {
-  if (!this.enabledOutput) return;
+    if (!this.enabledOutput) return;
 
-  var sceneEl = this.sceneEl;
-  var renderer = sceneEl.renderer;
-  var activeCamera = sceneEl.camera;
+    var sceneEl = this.sceneEl;
+    var renderer = sceneEl.renderer;
+    var activeCamera = sceneEl.camera;
 
-  if (!renderer || !activeCamera) return;
+    if (!renderer || !activeCamera) return;
 
-  activeCamera.updateMatrixWorld();
+    activeCamera.updateMatrixWorld();
 
-  this.cubeCamera.position.setFromMatrixPosition(activeCamera.matrixWorld);
+    this.cubeCamera.position.setFromMatrixPosition(activeCamera.matrixWorld);
 
-  this.cubeCamera.updateCubeMap(renderer, sceneEl.object3D);
+    this.cubeCamera.updateCubeMap(renderer, sceneEl.object3D);
 
-  renderer.autoClear = true;
-  renderer.clear();
-  renderer.render(this.quadScene, this.quadCamera);
+    renderer.autoClear = true;
+    renderer.clear();
+    renderer.render(this.quadScene, this.quadCamera);
+  },
 
+  getCurrentSiteName: function () {
+    if (Scene.nextSite) {
+      return Scene.nextSite;
+    }
+
+    if (Scene.currentSite) {
+      return Scene.currentSite;
+    }
+
+    return null;
+  },
+
+  broadcastNormalView: function (playerEl, cameraEl) {
+    if (!this.channel) return;
+
+    var playerPos = playerEl.object3D.position;
+    var playerRot = playerEl.object3D.rotation;
+
+    var cameraPos = cameraEl.object3D.position;
+    var cameraRot = cameraEl.object3D.rotation;
+
+    this.channel.postMessage({
+      site: this.getCurrentSiteName(),
+      currentSite: Scene.currentSite || null,
+      nextSite: Scene.nextSite || null,
+
+      playerPosition: {
+        x: playerPos.x,
+        y: playerPos.y,
+        z: playerPos.z
+      },
+      playerRotation: {
+        x: playerRot.x,
+        y: playerRot.y,
+        z: playerRot.z
+      },
+      cameraPosition: {
+        x: cameraPos.x,
+        y: cameraPos.y,
+        z: cameraPos.z
+      },
+      cameraRotation: {
+        x: cameraRot.x,
+        y: cameraRot.y,
+        z: cameraRot.z
+      }
+    });
+  },
+
+  applyCaveSync: function (playerEl, cameraEl) {
+    if (!this.latestSyncData) return;
+
+    var d = this.latestSyncData;
+
+    if (d.site && d.site !== Scene.currentSite && d.site !== this.lastRequestedSite) {
+      console.log('[EquirectangularOutput] Changing CAVE site to:', d.site);
+      this.lastRequestedSite = d.site;
+      Scene.onClickScene(d.site);
+      return;
+    }
+
+    playerEl.object3D.position.set(
+      d.playerPosition.x,
+      d.playerPosition.y,
+      d.playerPosition.z
+    );
+
+    playerEl.object3D.rotation.set(
+      d.playerRotation.x,
+      d.playerRotation.y,
+      d.playerRotation.z
+    );
+
+    cameraEl.object3D.position.set(
+      d.cameraPosition.x,
+      d.cameraPosition.y,
+      d.cameraPosition.z
+    );
+
+    cameraEl.object3D.rotation.set(
+      d.cameraRotation.x,
+      d.cameraRotation.y,
+      d.cameraRotation.z
+    );
+
+    playerEl.object3D.updateMatrixWorld(true);
+    cameraEl.object3D.updateMatrixWorld(true);
   }
 });
